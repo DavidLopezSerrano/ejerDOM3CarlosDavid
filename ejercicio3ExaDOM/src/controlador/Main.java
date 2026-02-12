a9d5508b345ab0c3007488d797f82db4586b4263
DavidLopezSerrano <davidlopezs.sanjosemlg@fundacionloyola.net>
2026-02-12 08:03:10 +0100

    Comentarios David

--- ejercicio3ExaDOM/src/controlador/Main.java
+++ ejercicio3ExaDOM/src/controlador/Main.java
@@ -1,3 +1,9 @@
+/*
+ * Ejercicio DOM-3 hecho por Carlos Michelena y David López
+ * Programa que genera mediante un evento un boton una textbox, y al darle a un boton se vuelve visible o invisible
+ * 11/02/2026
+ */
+
 package controlador;
 
 public class Main {
--- ejercicio3ExaDOM/src/vista/MainView.java
+++ ejercicio3ExaDOM/src/vista/MainView.java
@@ -1,3 +1,8 @@
+/*
+ * Ejercicio DOM-3 hecho por Carlos Michelena y David López
+ * Programa que genera mediante un evento un boton una textbox, y al darle a un boton se vuelve visible o invisible
+ * 11/02/2026
+ */
 package vista;
 
 import java.awt.FlowLayout;
